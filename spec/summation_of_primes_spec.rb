67214b14f3d69ce26fcb6e629b18dc72d1693638
Eleanor Lee <ellie007@gmail.com>
2014-03-06 10:57:35 -0600

    change the wording of the tests

--- spec/summation_of_primes_spec.rb
+++ spec/summation_of_primes_spec.rb
@@ -4,15 +4,15 @@ describe SumOfPrimes do
 
   let(:number) { SumOfPrimes.new }
 
-  xit "should filter out the primes from a sequence." do
+  xit "should sum the primes of a sequence." do
     number.filter_primes_from_sequence(20).should == 77
   end
 
-  xit "should filter out the primes from a sequence." do
+  xit "should sum the primes of a sequence." do
     number.sum_primes(2000000).should == 142913828922
   end
 
-  it "should filter out the primes from a sequence." do
+  it "should sum the primes of a sequence." do
     number.sum_primes(10000000).should == 3203324994356
   end
 
